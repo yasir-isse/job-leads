4eff0d1b1b89e21921c7c0dfdb7e7f880201da9c
Yasir Ahmed <code.yasir@gmail.com>
2022-09-20 08:24:47 +0300

    add header & hero components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
-  return <div className='App'>Hello World!</div>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
